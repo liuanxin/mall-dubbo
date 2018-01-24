3f9de10f573d4d93f6a33fd5bafb30c41da3cf07
liujie <liujie@wolianw.com>
2018-01-24 11:16:26 +0800

    fix

--- erp-common/src/main/java/com/github/common/sql/ShowSqlInterceptor.java
+++ erp-common/src/main/java/com/github/common/sql/ShowSqlInterceptor.java
@@ -1,17 +1,18 @@
 package com.github.common.sql;
 
-import com.alibaba.druid.sql.SQLUtils;
+import com.github.common.util.LogUtil;
+import com.github.common.util.U;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.ResultSetInternalMethods;
 import com.mysql.jdbc.Statement;
 import com.mysql.jdbc.StatementInterceptor;
-import com.github.common.util.LogUtil;
-import com.github.common.util.U;
 
 import java.sql.SQLException;
 import java.util.Properties;
 
 public class ShowSqlInterceptor implements StatementInterceptor {
+
+    private static final ThreadLocal<Long> TIME = new ThreadLocal<>();
 
     @Override
     public void init(Connection connection, Properties properties) throws SQLException {}
@@ -19,25 +20,26 @@ public class ShowSqlInterceptor implements StatementInterceptor {
     @Override
     public ResultSetInternalMethods preProcess(String sql, Statement statement,
                                                Connection connection) throws SQLException {
-        if (U.isBlank(sql) && statement != null) {
-            sql = statement.toString();
-            if (U.isNotBlank(sql) && sql.indexOf(':') > 0) {
-                // sql = SqlFormat.format(sql.substring(sql.indexOf(':') + 1).trim());
-                sql = SQLUtils.formatMySql(sql.substring(sql.indexOf(':') + 1).trim());
-            }
-        }
-        if (U.isNotBlank(sql)) {
-            if (LogUtil.SQL_LOG.isDebugEnabled()) {
-                LogUtil.SQL_LOG.debug("{}", sql);
-            }
-        }
+        TIME.set(System.currentTimeMillis());
         return null;
     }
 
     @Override
-    public ResultSetInternalMethods postProcess(String s, Statement statement,
+    public ResultSetInternalMethods postProcess(String sql, Statement statement,
                                                 ResultSetInternalMethods resultSetInternalMethods,
                                                 Connection connection) throws SQLException {
+        if (U.isBlank(sql) && statement != null) {
+            sql = statement.toString();
+            if (U.isNotBlank(sql) && sql.indexOf(':') > 0) {
+                sql = sql.substring(sql.indexOf(':') + 1).trim();
+            }
+        }
+        if (U.isNotBlank(sql)) {
+            if (LogUtil.SQL_LOG.isDebugEnabled() && !"SELECT 1".equalsIgnoreCase(sql)) {
+                // druid -> SQLUtils.formatMySql
+                LogUtil.SQL_LOG.debug("{}", SqlFormat.format(sql));
+            }
+        }
         return null;
     }
 
