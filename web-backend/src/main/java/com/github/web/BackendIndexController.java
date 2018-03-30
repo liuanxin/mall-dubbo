ac65d3d423bce5804ad408ae63c9574b0125eca2
liujie <liujie@wolianw.com>
2018-03-30 17:40:25 +0800

    fix

--- web-backend/src/main/java/com/github/web/BackendIndexController.java
+++ web-backend/src/main/java/com/github/web/BackendIndexController.java
@@ -1,18 +1,16 @@
 package com.github.web;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.github.common.Const;
 import com.github.common.json.JsonResult;
-import com.github.common.service.CommonService;
 import com.github.common.util.SecurityCodeUtil;
 import com.github.common.util.U;
+import com.github.global.model.Develop;
 import com.github.liuanxin.api.annotation.ApiIgnore;
+import com.github.liuanxin.api.annotation.ApiMethod;
 import com.github.liuanxin.api.annotation.ApiParam;
 import com.github.util.BackendDataCollectUtil;
 import com.github.util.BackendSessionUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
@@ -22,18 +20,6 @@ import java.io.IOException;
 @Controller
 public class BackendIndexController {
 
-    @Reference(version = Const.DUBBO_VERSION, lazy = true, check = false, timeout = Const.DUBBO_TIMEOUT)
-    private CommonService commonService;
-
-    @ResponseBody
-    @GetMapping("/example")
-    public JsonResult example(@RequestParam String name) {
-        if (U.isBlank(name)) {
-            name = U.EMPTY;
-        }
-        return JsonResult.success(commonService.example(name));
-    }
-
     @ResponseBody
     @GetMapping("/")
     public String index() {
@@ -41,6 +27,7 @@ public class BackendIndexController {
     }
 
     @ApiIgnore(false)
+    @ApiMethod(title = "枚举信息", develop = Develop.ORDER)
     @GetMapping("/enum")
     @ResponseBody
     public JsonResult enumList(@ApiParam(desc = "枚举类型. 不传则返回列表, type 与 枚举的类名相同, 忽略大小写") String type) {
