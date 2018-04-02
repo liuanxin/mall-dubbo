5e75b25ee2d3991b3a4c190e80cd940b2ec61ccf
liujie <liujie@wolianw.com>
2018-04-02 16:40:34 +0800

    fix

--- service-trace/trace-client/src/main/java/com/github/dubbo/trace/TraceUtil.java
+++ service-trace/trace-client/src/main/java/com/github/dubbo/trace/TraceUtil.java
@@ -1,13 +1,17 @@
 package com.github.dubbo.trace;
 
+import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 
 final class TraceUtil {
 
+    private static final String INTERFACE = "interface";
+    private static final String VERSION = "version";
+
     static String createSpanName(Invoker<?> invoker, Invocation invocation) {
-        return invoker.getUrl().getParameter("interface") + ":"
-                + invocation.getMethodName() + ":" + invoker.getUrl().getParameter("version")
-                + "(" + invoker.getUrl().getHost() + ")";
+        URL url = invoker.getUrl();
+        return String.format("%s:%s:%s(%s)", url.getParameter(INTERFACE),
+                invocation.getMethodName(), url.getParameter(VERSION), url.getHost());
     }
 }
