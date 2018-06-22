dec44c44c660463de3e3791e5c7f9f0fba6f53d8
liujie <liujie@wolianw.com>
2018-06-22 15:27:21 +0800

    fix

--- message-queue/queue-model/src/main/java/com/github/message/constant/MessageConst.java
+++ message-queue/queue-model/src/main/java/com/github/message/constant/MessageConst.java
@@ -6,7 +6,7 @@ public final class MessageConst {
     public static final String MODULE_NAME = "message";
 
     /** 当前模块说明. 当用在文档中时有用 */
-    public static final String MODULE_INFO = MODULE_NAME + "-消费订阅";
+    public static final String MODULE_INFO = MODULE_NAME + "-消息队列";
 
 
     // 用到的消息队列名
