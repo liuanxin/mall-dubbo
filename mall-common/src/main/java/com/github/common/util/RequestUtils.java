36dee7f2a53be398fd7cd5858ed63479924660fe
liujie <liujie@yunliketech.com>
2018-08-16 18:32:15 +0800

    fix

--- mall-common/src/main/java/com/github/common/util/RequestUtils.java
+++ mall-common/src/main/java/com/github/common/util/RequestUtils.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 
